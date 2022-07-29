f6d9369e54198a52d1c773023c393bb64fc0e7cb
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-28 21:25:32 -0700

    added the comments for the better understanding

--- src/IsPalindrome.java
+++ src/IsPalindrome.java
@@ -1,8 +1,13 @@
 public class IsPalindrome {
 
+    //if its palindrome then return 1, else 0
     public static int isPalindrome(String str){
+
+        // get two pointers from the beginning and the ending, and run a loop until they meet
+        // if both of the characters are equal then move the pointers and return 1, else 0
         int i=0;
         int j = str.length()-1;
+
         while(i<=j){
             if(str.charAt(i) == str.charAt(j)){
                 i++;
