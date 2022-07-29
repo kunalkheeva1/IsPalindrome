d9dfff06370759cdd9ec634c57046a3bf0fe32ce
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-28 21:23:31 -0700

    coded the method to check if the given string is palindrome or not

--- src/IsPalindrome.java
+++ src/IsPalindrome.java
@@ -1,6 +1,17 @@
 public class IsPalindrome {
 
-    public st
+    public static int isPalindrome(String str){
+        int i=0;
+        int j = str.length()-1;
+        while(i<=j){
+            if(str.charAt(i) == str.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return 0;
+            }
+        }return 1;
+    }
     public static void main(String[] args) {
 
     }
